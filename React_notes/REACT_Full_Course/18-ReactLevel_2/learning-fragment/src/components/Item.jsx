14bc3288aa0f9f8a58df418589f7dd08847e14fe
vipul <rajvipulraj401@gmail.com>
2024-01-26 16:19:36 +0530

    Added notes related to props and css modules

--- React_notes/REACT_Full_Course/18-ReactLevel_2/learning-fragment/src/components/Item.jsx
+++ React_notes/REACT_Full_Course/18-ReactLevel_2/learning-fragment/src/components/Item.jsx
@@ -7,9 +7,15 @@
 
 // we can also do via destructuring
 
+import styles from "./Item.module.css";
+
 const Item = ({ foods }) => {
   // THIS IS THE props object
-  return <li className="list-group-item">{foods}</li>;
+  return (
+    <li className={`${styles["kg-item"]}`}>
+      <span className={styles["kg-span"]}>{foods}</span>
+    </li>
+  );
 };
 
 export default Item;
